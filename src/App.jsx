fb217812419c3becedafe01422cb346355b3da43
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-06-16 14:24:21 -0300

    Realicé el ejercicio 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import  Ejercicio1  from './components/ejercicio1/Ejercicio1';
 import  Ejercicio2  from './components/ejercicio2/Ejercicio2';
 import  Ejercicio3  from './components/ejercicio3/Ejercicio3';
+import  Ejercicio4  from './components/ejercicio4/Ejercicio4';
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
     <h1>Mis ejercicios del TP de REACT</h1>
     {/*<Ejercicio1/>*/}
     {/*<Ejercicio2 titulo="my friend"/>*/}
-    {<Ejercicio3 titulo="my friend"/>}
+    {/*<Ejercicio3 titulo="my friend"/>*/}
+    {<Ejercicio4 titulo="my friend"/>}
     </>
   )
 }
---
+++ src/components/ejercicio4/Ejercicio4.jsx
@@ -0,0 +1,58 @@
+import { useState, useRef } from "react";
+
+const Ejercicio4 = () => {
+  const [tareas, setTareas] = useState([]);
+  
+  const $input = useRef();
+
+  const handleNuevaTarea = () => {
+    const nuevaTarea = $input.current.value;
+    if (nuevaTarea.trim()) {
+      const nuevoArreglo = [...tareas, nuevaTarea];
+      setTareas(nuevoArreglo);
+      $input.current.value = "";
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleNuevaTarea();
+    }
+  };
+
+  const handleEliminarTarea = (index) => {
+    const nuevoArreglo = tareas.filter((_, i) => i !== index);
+    setTareas(nuevoArreglo);
+  };
+
+  return (
+    <section className="d-flex justify-content-center align-items-center bg-white">
+      <div className="p-4 bg-white">
+        <h1 className="text-center mb-3">Bienvenido</h1>
+        <h3 className="text-center mb-4">Ingresa tus tareas</h3>
+        <div className="d-flex mb-3">
+          <input 
+            type="text" 
+            placeholder="Nueva tarea..." 
+            ref={$input} 
+            className="form-control me-2"
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <ul className="list-group">
+          {tareas.map((tarea, index) => (
+            <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+              {tarea}
+              <button onClick={() => handleEliminarTarea(index)} className="btn btn-danger btn-sm">
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Ejercicio4;
+
