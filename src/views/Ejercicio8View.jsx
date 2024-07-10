ddda1b3137ac93072b30dcc83ea75012220482b4
julieta vignoli <jvignoli20002@gmail.com>
2024-07-10 11:20:28 -0300

    Finalicé el ejercicio 8

--- src/views/Ejercicio8View.jsx
+++ src/views/Ejercicio8View.jsx
@@ -1,11 +1,22 @@
 import Ejercicio8 from '../components/ejercicio8/Ejercicio8';
 
+import { useEffect, useState } from 'react';
+
+const contactosLS = JSON.parse(localStorage.getItem('contactos')) || [];
+
+
 const Ejercicio8View = () => {
+  const [contactos, setContactos] = useState(contactosLS);
+
+  useEffect(()=>{
+    localStorage.setItem('contactos', JSON.stringify(contactos));
+  }, [contactos]);
+
   return (
     <>
     <h1>EJERCICIO 8</h1>
     <hr />
-    <Ejercicio8 />
+    <Ejercicio8 contactos={contactos} setContactos={setContactos}/>
     </>
   )
 }
