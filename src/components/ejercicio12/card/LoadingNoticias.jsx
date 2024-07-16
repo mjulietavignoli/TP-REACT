c8c2bc2f851461f144293ab97e8d22f378bfa6ab
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-07-16 09:43:35 -0300

    Agrego estilo al placeholder

--- src/components/ejercicio12/card/LoadingNoticias.jsx
+++ src/components/ejercicio12/card/LoadingNoticias.jsx
@@ -3,12 +3,15 @@ import './card.css';
 const LoadingNoticias = () => {
   return (
     <section className="row g-2 placeholder-glow">
-        <div className="col-6 col-md-4 placeholder w-full placeholder-size rounded-4">
-        </div>
-        <div className="col-6 col-md-4 placeholder w-full placeholder-size rounded-4">
-        </div>
-        <div className="col-6 col-md-4 placeholder w-full placeholder-size rounded-4">
-        </div>
+      <div className='col-6 col-md-4 placeholder-size'>
+        <div className="placeholder w-100 h-100 rounded-4"></div>
+      </div>
+      <div className='col-6 col-md-4'>
+        <div className="placeholder w-100 h-100 rounded-4"></div>
+      </div>
+      <div className='col-6 col-md-4'>
+        <div className="placeholder w-100 h-100 rounded-4"></div>
+      </div>
     </section>
   )
 }
