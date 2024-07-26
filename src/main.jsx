ce9f8b0f5d8d923d6f0bb983028142021c021348
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-07-26 16:53:14 -0300

    Borro archivos del ejercicio 14 porque empezaré en otra carpeta

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {router} from './constants/routes.jsx'
 import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
-import './App.css'
+import "./App.css"
 
 const queryClient = new QueryClient();
 
