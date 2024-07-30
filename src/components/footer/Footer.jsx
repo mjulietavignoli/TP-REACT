faa0ed36e53b8f1c059267344edc85c9969b0302
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-07-30 11:58:16 -0300

    Agrego footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="bg-body-tertiary">
+        <div className="container">
+            <p className="text-center text-dark m-3">Todos los derechos reservados &copy; 2023</p>
+        </div>
+    </div>
+  )
+}
+export default Footer
