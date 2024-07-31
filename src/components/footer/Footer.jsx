0ef425912a5d7dce60308b12ed118f21bb440453
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-07-31 08:25:45 -0300

    Agrego footer y todo terminado (creo)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="bg-body-tertiary">
         <div className="container">
-            <p className="text-center text-dark m-3">Todos los derechos reservados &copy; 2023</p>
+            <p className="text-center text-dark m-3">Todos los derechos reservados &copy;</p>
         </div>
     </div>
   )
